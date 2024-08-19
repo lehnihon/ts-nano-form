7595d2db863b614546c9f5b132bbddda90f6645b
Leandro Mac <lehnihon@hotmail.com>
2024-08-19 12:29:07 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({ rollupTypes: true, tsconfigPath: "./tsconfig.app.json" }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "TsNanoForm",
+      fileName: "ts-nano-form",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
